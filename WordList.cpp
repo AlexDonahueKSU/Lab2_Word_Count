dd24214eb82a82b16f5e4fec45fbefd21a1ea214
CrosscutX <crosscutcode@gmail.com>
2026-09-08 18:39:23 -0400

    Initial program

---
+++ WordList.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include "WordList.h" 
+using namespace std;
+
+// WordOccurrence definitions
+
+WordOccurrence::WordOccurrence(const string& word, int num) : word_(word), num_(num)
+{
+}
+
+bool WordOccurrence::matchWord(const string& word)
+{
+    return word_ == word;
+}
+
+void WordOccurrence::increment()
+{
+    num_++;
+}
+
+string WordOccurrence::getWord() const
+{
+    return word_;
+}
+
+int WordOccurrence::getNum() const
+{
+    return num_;
+}
+
+// WordList definitions
+
+WordList::WordList()
+    : wordArray_(nullptr), size_(0)
+{
+}
+
+WordList::WordList(const WordList& other)
+    : wordArray_(nullptr), size_(other.size_)
+{
+    if (size_ > 0)
+    {
+        wordArray_ = new WordOccurrence[size_];
+
+        for (int i = 0; i < size_; i++)
+        {
+            wordArray_[i] = other.wordArray_[i];
+        }
+    }
+}
+
+WordList::~WordList()
+{
+    delete[] wordArray_;
+}
+
+WordList& WordList::operator=(const WordList& other)
+{
+    if (this != &other)
+    {
+        WordOccurrence* newArray = nullptr;
+
+        if (other.size_ > 0)
+        {
+            newArray = new WordOccurrence[other.size_];
+
+            for (int i = 0; i < other.size_; i++)
+            {
+                newArray[i] = other.wordArray_[i];
+            }
+        }
+
+        delete[] wordArray_;
+
+        wordArray_ = newArray;
+        size_ = other.size_;
+    }
+
+    return *this;
+}
+
+void WordList::addWord(const string& word)
+{
+    for (int i = 0; i < size_; i++)
+    {
+        if (wordArray_[i].matchWord(word))
+        {
+            wordArray_[i].increment();
+            return;
+        }
+    }
+
+    WordOccurrence* newArray = new WordOccurrence[size_ + 1];
+
+    for (int i = 0; i < size_; i++)
+    {
+        newArray[i] = wordArray_[i];
+    }
+
+    newArray[size_] = WordOccurrence(word, 1);
+
+    delete[] wordArray_;
+
+    wordArray_ = newArray;
+    size_++;
+}
+
+void WordList::printList()
+{
+    for (int i = 0; i < size_; i++)
+    {
+        cout << wordArray_[i].getWord()
+             << ": "
+             << wordArray_[i].getNum()
+             << endl;
+    }
+}
---
+++ WordListDriver.cpp
@@ -0,0 +1,37 @@
+// testing the implementation of class WordList 
+// Alex Donahue
+// 9/08/2026
+
+#include <iostream>
+#include "WordList.h" // class definitions 
+using namespace std; 
+
+void testfunc(WordList); // function to test pass-by-value for WordList class 
+
+int main()
+{ 
+	WordList wl; 
+	
+	// testing regular member functions 
+	wl.addWord("one"); 
+	wl.addWord("one"); // adding duplicate 
+	wl.addWord("two"); 
+	wl.addWord("three"); 
+	cout << "word list is:\n"; 
+	wl.printList(); 
+	
+	WordList wl2, wl3; 
+	
+	wl3=wl2=wl; // testing overloaded assignment 
+	wl3=wl3; // testing protection against self-assingment 
+	testfunc(wl); // testing copy constructor 
+	wl.printList(); // if destructor is implemented correctly 
+		     // this should print properly after testfunc completes 
+} // tests pass-by-value for object of class varArray 
+
+void testfunc(WordList myList)
+{ 
+	// copy constructor is invoked on "myList" 
+	cout << "word list in testfunc: \n"; 
+	myList.printList(); 
+} // destructor is invoked when "myList" goes out of scope 
